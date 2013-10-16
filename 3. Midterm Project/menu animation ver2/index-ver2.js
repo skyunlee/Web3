961a48745b476c5b9306d59f0388093f73fe3e7b
skyunlee <seungkyunlee@Seungkyuns-MacBook-Air.local>
2013-10-16 18:48:06 -0400

    menu thumbnails drop at different speed

--- 3. Midterm Project/menu animation ver2/index-ver2.js
+++ 3. Midterm Project/menu animation ver2/index-ver2.js
@@ -9,5 +9,14 @@ function menuShows(){
 
 	if(scrollValueY > menuContainerPosY){
 		// console.log("you reached the menu div");
+
+		var thm_delay1 = document.getElementsByClassName("thm_delay1").item(0);
+		thm_delay1.classList.add("is-active");
+		var thm_delay2 = document.getElementsByClassName("thm_delay2").item(0);
+		thm_delay2.classList.add("is-active");
+		var thm_delay3 = document.getElementsByClassName("thm_delay3").item(0);
+		thm_delay3.classList.add("is-active");
+		var thm_delay4 = document.getElementsByClassName("thm_delay4").item(0);
+		thm_delay4.classList.add("is-active");
 	}
 }
