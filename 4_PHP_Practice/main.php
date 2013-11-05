9e0c584ace1172f8916819af453925465d107eb0
skyunlee <seungkyunlee@Seungkyuns-MacBook-Air.local>
2013-11-05 00:13:46 -0500

    array is working

--- 4_PHP_Practice/main.php
+++ 4_PHP_Practice/main.php
@@ -5,23 +5,25 @@
 	<?php
 		//create array of all the menu items
 		$pastries = array(
-			'blueberry pie' => array('price'=>3.50, 'calories'=>500, 'filename'=>'blueberrypie.png'),
-			'brownies' => array('price'=>2.50, 'calories'=>350, 'filename'=>'brownies.png'),
-			'cakepop' => array('price'=>2.25, 'calories'=>320, 'filename'=>'cakepop.png'),
-			'caramelcoffee' => array('price'=>2.75, 'calories'=>450, 'filename'=>'caramelcoffee.png'),
-			'chocolatecroissant' => array('price'=>2.25, 'calories'=>320, 'filename'=>'chocolatecroissant.png'),
-			'cupcake' => array('price'=>3.45, 'calories'=>600, 'filename'=>'cupcake.png'),
-			'donuts' => array('price'=>1.25, 'calories'=>220, 'filename'=>'donuts.png'),
-			'hotchoco' => array('price'=>2.95, 'calories'=>385, 'filename'=>'hotchoco.png'),
-			'icedcoffee' => array('price'=>2.15, 'calories'=>10, 'filename'=>'icedcoffee.png'),
-			'latte' => array('price'=>3.25, 'calories'=>290, 'filename'=>'latte.png'),
-			'macarons' => array('price'=>2.65, 'calories'=>100, 'filename'=>'macarons.png'),
-			'tart' => array('price'=>4.50, 'calories'=>450, 'filename'=>'tart.png')
+			'blueberry pie' => array('price'=>3.50, 'calories'=>500),
+			'brownies' => array('price'=>2.50, 'calories'=>350),
+			'cakepop' => array('price'=>2.25, 'calories'=>320),
+			'caramelcoffee' => array('price'=>2.75, 'calories'=>450),
+			'chocolatecroissant' => array('price'=>2.25, 'calories'=>320),
+			'cupcake' => array('price'=>3.45, 'calories'=>600),
+			'donuts' => array('price'=>1.25, 'calories'=>220),
+			'hotchoco' => array('price'=>2.95, 'calories'=>385),
+			'icedcoffee' => array('price'=>2.15, 'calories'=>10),
+			'latte' => array('price'=>3.25, 'calories'=>290),
+			'macarons' => array('price'=>2.65, 'calories'=>100),
+			'tart' => array('price'=>4.50, 'calories'=>450)
 			);
 
-		header('Access-Control-Origin: *');
-		header('Content-Type: application/json');
-		echo json_encode($pastries);
+		echo $pastries['brownies']['price'];
+
+		// header('Access-Control-Origin: *');
+		// header('Content-Type: application/json');
+		// echo json_encode($pastries);
 
 		// echo "HI";
 
