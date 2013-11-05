b9731bc12e8c5553d7333c759bb3f425ba070219
skyunlee <seungkyunlee@Seungkyuns-MacBook-Air.local>
2013-11-05 03:40:13 -0500

    pastry and drink menus display separatedly

--- 4_PHP_Practice/main.php
+++ 4_PHP_Practice/main.php
@@ -7,22 +7,22 @@
 
 	//array for pastries
 	$menuLists['pastries'] = array(
-		'Blueberry Pie' => array('price'=>3.50, 'calories'=>500),
 		'Brownies' => array('price'=>2.50, 'calories'=>350),
-		'Cakepop' => array('price'=>2.25, 'calories'=>320),
+		'Cake Pop' => array('price'=>2.25, 'calories'=>320),
 		'Chocolate Croissant' => array('price'=>2.25, 'calories'=>320),
 		'Cupcake' => array('price'=>3.45, 'calories'=>600),
 		'Donuts' => array('price'=>1.25, 'calories'=>220),
 		'Macarons' => array('price'=>2.65, 'calories'=>100),
-		'Tart' => array('price'=>4.50, 'calories'=>450)
+		'Fruit Tart' => array('price'=>4.50, 'calories'=>450),
+		'Blueberry Pie' => array('price'=>3.50, 'calories'=>500)
 		);
 
 	//array for drinks
 	$menuLists['drinks'] = array(
 		'Latte' => array('price'=>3.25, 'calories'=>290),
 		'Iced Coffee' => array('price'=>2.15, 'calories'=>10),
-		'Hot Chocolate' => array('price'=>2.95, 'calories'=>385),
-		'Caramel Coffee' => array('price'=>2.75, 'calories'=>450)
+		'Caramel Coffee' => array('price'=>2.75, 'calories'=>450),
+		'Hot Chocolate' => array('price'=>2.95, 'calories'=>385)
 		);
 
 	//echo $menuLists['pastries']['cakepop']['calories'];
@@ -35,18 +35,40 @@
 
 <article>
 	<h2>Pastries</h2>
- 	<?php
+	<?php 
+		$pastryList = $menuLists['pastries'];
+		foreach($pastryList as $pastry => $detail){
+			echo $pastry; ?> <br> <?php
+		}
+	?>
+
+ <!-- 	<?php
 		foreach($menuLists as $menuList => $itemList){
 			foreach ($itemList as $item => $itemDetail){
 				echo $item; ?> <br> <?php
 			}
 		}
 		// endforeach;
-	?> 
+	?>  -->
 
 	<h2>Coffee Drinks</h2>
 	<?php
+		$drinkList = $menuLists['drinks'];
+		foreach($drinkList as $drink => $detail){
+			echo $drink; ?> <br> <?php
+		}
 	?>
+
+
+	<!--code below didn't work-->
+<!-- 	<?php
+		foreach(array_slice(menuLists, 1) as $itemList){
+			foreach($itemList as $item => $itemDetail){
+				echo $item;
+			}
+		}
+	?> -->
+
 </article>
 
 
