25e2c14624a4f0cdd72370c4638db45a01319277
skyunlee <seungkyunlee@Seungkyuns-MacBook-Air.local>
2013-11-04 22:38:10 -0500

    added array

--- 4_PHP_Practice/main.php
+++ 4_PHP_Practice/main.php
@@ -4,7 +4,18 @@
 
 	<?php
 		$pastries = array(
-			
+			'blueberry pie' => array('price'=>3.50, 'calories'=>500, 'filename'=>'blueberrypie.png'),
+			'brownies' => array('price'=>2.50, 'calories'=>350, 'filename'=>'brownies.png'),
+			'cakepop' => array('price'=>2.25, 'calories'=>320, 'filename'=>'cakepop.png'),
+			'caramelcoffee' => array('price'=>2.75, 'calories'=>450, 'filename'=>'caramelcoffee.png'),
+			'chocolatecroissant' => array('price'=>2.25, 'calories'=>320), 'filename'=>'chocolatecroissant.png'),
+			'cupcake' => array('price'=>3.45, 'calories'=>600), 'filename'=>'cupcake.png'),
+			'donuts' => array('price'=>1.25, 'calories'=>220), 'filename'=>'donuts.png'),
+			'hotchoco' => array('price'=>2.95, 'calories'=>385), 'filename'=>'hotchoco.png'),
+			'icedcoffee' => array('price'=>2.15, 'calories'=>10), 'filename'=>'icedcoffee.png'),
+			'latte' => array('price'=>3.25, 'calories'=>290), 'filename'=>'latte.png'),
+			'macarons' => array('price'=>2.65, 'calories'=>100), 'filename'=>'macarons.png'),
+			'tart' => array('price'=>4.50, 'calories'=>450), 'filename'=>'tart.png'),
 			);
 
 	?>
